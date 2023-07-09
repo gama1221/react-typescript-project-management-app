ffa6b1911b9a73d6582888266f27af0c20786ed4
getinet amare <getinetamare@gmail.com>
2023-07-09 11:10:17 +0300

    Add default files

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import ProjectsPage from './projects/ProjectsPage';
+
+const App = ()=> {
+  return (
+    <div className='container'>
+      <ProjectsPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/projects/ProjectsPage.tsx
@@ -0,0 +1,15 @@
+import { MOCK_PROJECTS } from "./MockProjects";
+
+const ProjectsPage = () => {
+    return (
+        <>
+            <h1>Projects</h1>
+            <pre>
+                {
+                    JSON.stringify(MOCK_PROJECTS, null, ' ')
+                }
+            </pre>
+        </>
+    )
+}
+export default ProjectsPage;
